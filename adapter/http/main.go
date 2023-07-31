ebf675d9899ead77302ab629076e59d1c2605057
Willian Silva <silva.williansantos@gmail.com>
2023-07-31 10:15:59 -0300

    refactor: :recycle: switch gin to go-chi

--- adapter/http/main.go
+++ adapter/http/main.go
@@ -3,15 +3,19 @@ package main
 import (
 	"context"
 	"fmt"
+	"net/http"
+	"time"
 
-	"github.com/gin-gonic/gin"
-	"github.com/willian2s/clean-go/adapter/http/middleware"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
+	"github.com/go-chi/httprate"
+
 	"github.com/willian2s/clean-go/adapter/postgres"
 	"github.com/willian2s/clean-go/config"
 	"github.com/willian2s/clean-go/di"
 
-	swaggerFiles "github.com/swaggo/files"
-	ginSwagger "github.com/swaggo/gin-swagger"
+	httpSwagger "github.com/swaggo/http-swagger/v2"
 	_ "github.com/willian2s/clean-go/adapter/http/docs"
 )
 
@@ -32,27 +36,28 @@ func main() {
 	postgres.RunMigrations()
 	productService := di.ConfigProductDI(conn)
 
-	mode := config.EnvConfigs.Mode
-	gin.SetMode(mode)
+	app := chi.NewRouter()
 
-	router := gin.Default()
-	router.SetTrustedProxies(nil)
+	app.Use(middleware.RequestID)
+	app.Use(middleware.Recoverer)
+	app.Use(middleware.Logger)
+	app.Use(httprate.LimitByIP(100, 1*time.Minute))
+	app.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
 
-	router.Use(middleware.RateLimit, gin.Recovery())
-	router.Use(middleware.Cors)
+	app.Route("/product", func(r chi.Router) {
+		r.Get("/", productService.Fetch)
+		r.Post("/", productService.Create)
+	})
 
-	product := router.Group("/product")
-	{
-		product.POST("/", func(c *gin.Context) {
-			productService.Create(c.Writer, c.Request)
-		})
-		product.GET("/", func(c *gin.Context) {
-			productService.Fetch(c.Writer, c.Request)
-		})
-	}
-
-	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+	app.Get("/swagger/*", httpSwagger.WrapHandler)
 
 	port := config.EnvConfigs.ServerPort
-	router.Run(fmt.Sprintf(":%s", port))
+	http.ListenAndServe(fmt.Sprintf(":%s", port), app)
 }
